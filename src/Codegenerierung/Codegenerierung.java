80e2a379a5a8754ed038e27f4b63c2b03445c281
Leo Schnüll <leo.schnuell@atos.net>
2023-07-12 17:06:34 +0200

    added binary and unary

--- src/Codegenerierung/Codegenerierung.java
+++ src/Codegenerierung/Codegenerierung.java
@@ -3,7 +3,7 @@ package Codegenerierung;
 
 import Expr.*;
 
-import org.antlr.runtime.LegacyCommonTokenStream;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import statementExpressions.*;
@@ -21,7 +21,14 @@ public class Codegenerierung {
     //Expressions
 
     public void visit(ACharacter aCharacter){
-
+        char aCharacterValue = aCharacter.getValue();
+        if (aCharacterValue <= Byte.MAX_VALUE) {
+            methodvisitor.visitIntInsn(Opcodes.BIPUSH, aCharacterValue);
+        } else if (aCharacterValue <= Short.MAX_VALUE) {
+            methodvisitor.visitIntInsn(Opcodes.SIPUSH, aCharacterValue);
+        } else {
+            methodvisitor.visitLdcInsn(aCharacterValue);
+        }
     }
 
     public void visit(AInteger aInteger) {
@@ -41,8 +48,80 @@ public class Codegenerierung {
     }
 
     public void visit(Binary binary){
-
+        switch (binary.getOperator()) {
+            case PLUS, MINUS, MULTIPLY, DIVIDE, MODULUS -> addMathCodes(binary);
+            case GREATER_THAN, LESS_THAN, GREATER_THAN_OR_EQUAL_TO, LESS_THAN_OR_EQUAL_TO, EQUAL_TO, NOT_EQUAL_TO, AND, OR -> addBooleanCodes(binary);
+        }
     }
+
+    private void addMathCodes(Binary binary) {
+        binary.getLeft().bevisited(this);
+        binary.getRight().bevisited(this);
+
+        switch (binary.getOperator()) {
+            case PLUS -> methodvisitor.visitInsn(Opcodes.IADD);
+            case MINUS -> methodvisitor.visitInsn(Opcodes.ISUB);
+            case MULTIPLY -> methodvisitor.visitInsn(Opcodes.IMUL);
+            case DIVIDE -> methodvisitor.visitInsn(Opcodes.IDIV);
+            case MODULUS -> methodvisitor.visitInsn(Opcodes.IREM);
+
+        }
+    }
+
+    private void addBooleanCodes(Binary binary) {
+
+        Label trueLabel = new Label();
+        Label falseLabel = new Label();
+        Label end = new Label();
+
+        switch (binary.getOperator()) {
+            case AND -> {
+                binary.getLeft().bevisited(this);
+                methodvisitor.visitJumpInsn(Opcodes.IFEQ, falseLabel); // one false is enough
+                binary.getRight().bevisited(this);
+                methodvisitor.visitJumpInsn(Opcodes.IFEQ, falseLabel);
+            }
+            case OR -> {
+                binary.getLeft().bevisited(this);
+                methodvisitor.visitJumpInsn(Opcodes.IFNE, trueLabel); // one true is enough
+                binary.getRight().bevisited(this);
+                methodvisitor.visitJumpInsn(Opcodes.IFEQ, falseLabel);
+            }
+            case GREATER_THAN -> {
+                binary.getLeft().bevisited(this);
+                binary.getRight().bevisited(this);
+                methodvisitor.visitJumpInsn(Opcodes.IF_ICMPLE, falseLabel);
+            }
+            case LESS_THAN -> {
+                binary.getLeft().bevisited(this);
+                binary.getRight().bevisited(this);
+                methodvisitor.visitJumpInsn(Opcodes.IF_ICMPGE, falseLabel);
+            }
+            case GREATER_THAN_OR_EQUAL_TO -> {
+                binary.getLeft().bevisited(this);
+                binary.getRight().bevisited(this);
+                methodvisitor.visitJumpInsn(Opcodes.IF_ICMPLT, falseLabel);
+            }
+            case LESS_THAN_OR_EQUAL_TO -> {
+                binary.getLeft().bevisited(this);
+                binary.getRight().bevisited(this);
+                methodvisitor.visitJumpInsn(Opcodes.IF_ICMPGT, falseLabel);
+            }
+
+           // case EQUAL_TO and case NOT_EQUAL_TO missing, I didn't know how
+
+        }
+
+        methodvisitor.visitLabel(trueLabel);
+        methodvisitor.visitInsn(Opcodes.ICONST_1);
+        methodvisitor.visitJumpInsn(Opcodes.GOTO, end);
+
+        methodvisitor.visitLabel(falseLabel);
+        methodvisitor.visitInsn(Opcodes.ICONST_0);
+
+        methodvisitor.visitLabel(end);
+    }
+
 
     public void visit(IdExpr idExpr){
 
@@ -60,8 +139,22 @@ public class Codegenerierung {
 
     }
 
-    public void visit(Unary unary){
+    public void visit(Unary unary){ //accepts only Not as an unary input
 
+        Label trueLabel = new Label();
+        Label falseLabel = new Label();
+        Label end = new Label();
+        unary.getExpr().bevisited(this);
+        methodvisitor.visitJumpInsn(Opcodes.IFNE, falseLabel); //Jump to falseinsert in case the value is true
+
+        methodvisitor.visitLabel(trueLabel);
+        methodvisitor.visitInsn(Opcodes.ICONST_1);
+        methodvisitor.visitJumpInsn(Opcodes.GOTO, end);
+
+        methodvisitor.visitLabel(falseLabel);
+        methodvisitor.visitInsn(Opcodes.ICONST_0);
+
+        methodvisitor.visitLabel(end);
     }
 
     public void visit(InstVar instVar) {
@@ -136,17 +229,5 @@ public class Codegenerierung {
 
 
     //Helpers
-    private void visitArithmetic(Binary binary) {
-        binary.getLeft().bevisited(this);
-        binary.getRight().bevisited(this);
 
-        switch (binary.getOperator()) {
-            case PLUS -> methodvisitor.visitInsn(Opcodes.IADD);
-            case MINUS -> methodvisitor.visitInsn(Opcodes.ISUB);
-            case MULTIPLY -> methodvisitor.visitInsn(Opcodes.IMUL);
-            case DIVIDE -> methodvisitor.visitInsn(Opcodes.IDIV);
-            case MODULUS -> methodvisitor.visitInsn(Opcodes.IREM);
-
-        }
-    }
 }
