1f5177350a456f2a3b3894813e40c965f2403f8c
Paul Antoni <paul.antoni@siemens-healthineers.com>
2023-07-14 23:24:35 +0200

    chore: cleanup and comments

--- src/TypeChecking/ClassChecker.java
+++ src/TypeChecking/ClassChecker.java
@@ -6,24 +6,22 @@ import jdk.jshell.spi.ExecutionControl;
 import java.util.List;
 
 public class ClassChecker {
-    private String name;
     private List<AField> fields;
     private List<Method> methods;
     private MethodChecker methodChecker;
 
 
     public ClassChecker(Clazz clazz, List<AField> fields, List<Method> methods) {
-        name = clazz.getName();
         this.fields = fields;
         this.methods = methods;
         this.methodChecker = new MethodChecker(clazz);
-        //since we don't have constructors, they cannot be type checked
+        //since we don't have constructors, they cannot be type-checked
     }
 
-    public AField check(AField field) throws Exception {//todo: fix strings
+    public AField check(AField field) throws Exception {//todo: fix strings (currently not implemented as type)
         String type = field.getType().toString();
         switch (type) {
-            case "String" -> /*field.setType(AType.nonexistantapparently)*/ throw(new ExecutionControl.NotImplementedException("string not implemented"));
+            case "String" -> /*field.setType(AType.nonexistentapparently)*/ throw(new ExecutionControl.NotImplementedException("string not implemented"));
             case "char" -> field.setType(AType.CHAR);
             case "int" -> field.setType(AType.INT);
             case "boolean" -> field.setType(AType.BOOLEAN);
@@ -38,11 +36,10 @@ public class ClassChecker {
         return methodChecker.check(method);
     }
 
-    public Method check(String methodId) throws Exception {// todo: is this even used? does not seem so
+    public Method check(String methodId) throws Exception {// could be used, if only the methodID were available, but currently unused
         for (Method method : methods) {
             if (method.getId().equals(methodId)) {
-                Method checkedMethod = check(method);
-                return checkedMethod;
+                return check(method);
             }
         }
         throw new Exception();
